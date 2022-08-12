0bd7f9a4503ca54e774ae83a8b7cf32058841140
MonsterBasket <blazza1@hotmail.com>
2022-08-12 16:05:43 +1000

    tests complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { image } from "../data/data";
-
+/* eslint no-undef: "off"*/
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Edison style</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
